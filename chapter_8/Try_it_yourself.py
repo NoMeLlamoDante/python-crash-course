ec400dca7ff0e9465c35aa1e20c49d501f0e3232
NoMeLlamoDante <eo.zm24@outlook.com>
2024-11-03 20:31:40 -0600

    C8 TIY 8-3 to 8-5

--- chapter_8/Try_it_yourself.py
+++ chapter_8/Try_it_yourself.py
@@ -10,4 +10,28 @@ def display_message():
 def favorite_books(title):
     print(f"One of my favorite books is {title.title()}")
     
-print("the little prince")
+# print("the little prince")
+
+#8-3 T-Shirt
+def make_shirt(text, size):
+    print(f"you order a {size} size, with the text: '{text}'")
+
+# make_shirt("Hello world", "Large")
+# make_shirt(size="Medium", text="I don't wanna be here")
+
+#8-4 Large Shirts
+def make_shirt(text="I love Python", size='Large'):
+    print(f"you order a {size} size, with the text: '{text}'")
+
+# make_shirt()
+# make_shirt(size="Medium")
+# make_shirt("Free PyCharm", "Small")
+# make_shirt(size="XS", text="God's save to C")
+
+#8-5 Cities
+def describe_city(city, country='japan'):
+    print(f'{city.title()} is in {country.title()}')
+
+describe_city('tokyo')
+describe_city('guadalajara', 'méxico')
+describe_city(country='canada', city='toronto')
