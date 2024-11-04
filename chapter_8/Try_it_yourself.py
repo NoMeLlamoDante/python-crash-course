d8532afae2e70bfef21f28f4bea982420c6ede7b
NoMeLlamoDante <eo.zm24@outlook.com>
2024-11-03 19:11:59 -0600

    C8 TIY 8-1 to 8-2

---
+++ chapter_8/Try_it_yourself.py
@@ -0,0 +1,13 @@
+#8-1 Message
+
+def display_message():
+    """sample display"""
+    print("in this chapter you can learn functions and parameters")
+    
+# display_message()
+
+#8-2 Favorite Book
+def favorite_books(title):
+    print(f"One of my favorite books is {title.title()}")
+    
+print("the little prince")
