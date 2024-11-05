f9c047bd38cb568f851d33408aea6f0347f47444
NoMeLlamoDante <eo.zm24@outlook.com>
2024-11-05 16:54:37 -0600

    C8 try it yourself 8-6 to 8-8

--- chapter_8/Try_it_yourself.py
+++ chapter_8/Try_it_yourself.py
@@ -36,6 +36,56 @@ def describe_city(city, country='japan'):
     """Display a phrase using a positional parameter and a keyword argument"""
     print(f'{city.title()} is in {country.title()}')
 
-describe_city('tokyo')
-describe_city('guadalajara', 'méxico')
-describe_city(country='canada', city='toronto')
+# describe_city('tokyo')
+# describe_city('guadalajara', 'méxico')
+# describe_city(country='canada', city='toronto')
+
+#8-6 City names
+def city_country(city, country):
+    return f"{city.title()}, {country.title()}"
+
+# print(city_country('zaragoza', 'spain'))
+# print(city_country('london', 'united kingdom'))
+# print(city_country('lima', 'peru'))
+
+#8-7 Album
+def make_album(artist, title, songs=None):
+    return {
+        'album artist': artist, 
+        'album title':title, 
+        'songs': songs
+    }
+
+# print(make_album('michael jackson', 'thriller',7))
+# print(make_album('ed sheeran', '+'))
+# print(make_album('the beetles', "Sgt. Pepper's Lonely Hearts Club Band"))
+
+#8-8 User Albums
+
+
+while True:
+    print("Let's add album info:")
+    print("(enter 'q' to quit)")
+    
+    artist = input("Enter artist album: ")
+    if artist == 'q':
+        break
+    
+    title = input("Enter album title: ")
+    if title == 'q':
+        break
+    
+    no_songs = input("Enter number of songs: ")
+    if no_songs == 'q':
+        break
+    if no_songs == "":
+        no_songs = 0
+    else: 
+        no_songs = int(no_songs)
+    
+    if artist and title and no_songs > 0:
+        album = make_album(artist, title, no_songs)
+    elif artist and title:  
+        album = make_album(artist, title)
+    
+    print(album)
