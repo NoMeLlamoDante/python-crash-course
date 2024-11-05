28a2cf55849039eaef53d836cbc0e0eb4ee631a5
NoMeLlamoDante <eo.zm24@outlook.com>
2024-11-05 16:20:04 -0600

    C8 greeter example

--- chapter_8/greeter.py
+++ chapter_8/greeter.py
@@ -1,7 +1,21 @@
-def greet_user(username):
-    """Display a simple greeting"""
-    print(f"Hello {username.title()}!")
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formated"""
+    full_name = f'{first_name} {last_name}'
+    return full_name.title()
+
+#This is an infinity loop!
+while True:
+    print("\nPlease tell me your name:")
+    print("(Enter 'q' at any time to quit)")
     
-greet_user('reiner')
-greet_user('sarah')
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    
+    l_name = input("Last name: ")
+    if f_name == 'q':
+        break
+    
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name.title()}")
 
