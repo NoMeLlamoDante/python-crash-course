f6e8ff16e563d5ad905ae2240bf9f120fbe49604
NoMeLlamoDante <eo.zm24@outlook.com>
2024-11-06 19:26:09 -0600

    C8 pizza module example

---
+++ chapter_8/making_pizza.py
@@ -0,0 +1,9 @@
+# import pizza
+# import pizza as p
+from pizza import make_pizza as mp
+# from pizza import *
+# pizza.make_pizza(16, 'peperoni')
+# p.make_pizza(16, 'peperoni')
+# make_pizza(16, 'peperoni')
+mp(16, 'peperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
--- chapter_8/pizza.py
+++ chapter_8/pizza.py
@@ -3,6 +3,3 @@ def make_pizza(size, *toppings):
     print(f"\nMaking a {size}-inch pizza with the following topping")
     for topping in toppings:
         print(f"-{topping}")
-
-make_pizza(16,'peperoni')
-make_pizza(12,'mushrooms', 'green pepers', 'extra cheese')
