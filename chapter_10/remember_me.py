af7c7c56de8455487c387df7f7a2df6e7c04f76b
NoMeLlamoDante <eo.zm24@outlook.com>
2024-11-11 21:58:53 -0600

    refactoring 2

--- chapter_10/remember_me.py
+++ chapter_10/remember_me.py
@@ -1,18 +1,25 @@
 from pathlib import Path
 import json
 
-
-def greet_user():
-    """Greet the user by name."""
-    path = Path("chapter_10/username.json")    
+def get_stored_user(path):
+    """Get stored username if available."""
     if path.exists():
         contents = path.read_text()
         username = json.loads(contents)
-        print(f"Welcome back {username}")
+        return username
+    else:
+        return None
+
+def greet_user():
+    """Greet the user by name."""
+    path = Path("chapter_10/username.json")
+    username = get_stored_user(path)
+    if username:
+        print(f"welcome back, {username}!")
     else:
         username = input("Whats your name? ")
         contents = json.dumps(username)
         path.write_text(contents)
-        print(f"We'll remember you when you come back {username}")
+        print(f"We'll remember you when you come back {username}!")
 
 greet_user()
