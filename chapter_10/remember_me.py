b9aef8359dd72e6157699eb794695dad79885886
NoMeLlamoDante <eo.zm24@outlook.com>
2024-11-11 21:47:16 -0600

    C10 json Examples

---
+++ chapter_10/remember_me.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import json
+
+path = Path("chapter_10/username.json")
+
+if path.exists():
+    contents = path.read_text()
+    username = json.loads(contents)
+    print(f"Welcome back {username}")
+
+else:
+    username = input("Whats your name? ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+    print(f"We'll remember you when you come back {username}")
